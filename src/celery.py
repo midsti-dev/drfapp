eae659bd489f93c3bfc633d365562b815c1c4ac5
midsti-dev <230103378@sdu.edu.kz>
2025-07-23 15:05:20 +0500

    celery django setup

--- src/celery.py
+++ src/celery.py
@@ -1,7 +1,11 @@
+import django
 from celery import Celery
 import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.settings")
+
+django.setup()
+
 app = Celery("src")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
